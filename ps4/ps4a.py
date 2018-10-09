b9e38ca5d88f4501dcb01c761e65fced88df1d9b
BearCleverProud <conghao.xiong@gmail.com>
2018-10-09 16:28:29 +0800

    Inperfect finish

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -1,6 +1,6 @@
 # Problem Set 4A
-# Name: <your name here>
-# Collaborators:
+# Name: <BearCleverProud>
+# Collaborators:None
 # Time Spent: x:xx
 
 def get_permutations(sequence):
@@ -8,7 +8,7 @@ def get_permutations(sequence):
     Enumerate all permutations of a given string
 
     sequence (string): an arbitrary string to permute. Assume that it is a
-    non-empty string.  
+    non-empty string.
 
     You MUST use recursion for this part. Non-recursive solutions will not be
     accepted.
@@ -22,19 +22,31 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
-
-    pass #delete this line and replace with your code here
+    length=len(sequence)
+    result=[]
+    if length==1:
+        return sequence
+    else:
+        result.append(sequence)
+        sequence_list=list(sequence)
+        temp=(sequence_list[0]+x for x in get_permutations(''.join(sequence_list[1:])))
+        result.extend(temp)
+        for i in range(1,length):
+            sequence_list[i],sequence_list[0]=sequence_list[0],sequence_list[i]
+            result.append(''.join(sequence_list))
+            temp=(sequence_list[0]+x for x in get_permutations(''.join(sequence_list[1:])))
+            result.extend(temp)
+            sequence_list[i],sequence_list[0]=sequence_list[0],sequence_list[i]
+        result=list(set(result))
+        return result
 
 if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
-    
+    #EXAMPLE
+    example_input = 'abcd'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
+
 #    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
+#    to be three characters or fewer as you will have n! permutations for a
 #    sequence of length n)
-
-    pass #delete this line and replace with your code here
-
