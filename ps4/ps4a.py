e0d14939bf6441fa81050e7c9d7431d467ee6581
BearCleverProud <conghao.xiong@gmail.com>
2018-10-09 16:50:44 +0800

    Little change

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -37,7 +37,7 @@ def get_permutations(sequence):
 
 if __name__ == '__main__':
     #EXAMPLE
-    example_input = 'abcdefgh'
+    example_input = 'abc'
     print('Input:', example_input)
     print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
     print('Actual Output:', get_permutations(example_input))
